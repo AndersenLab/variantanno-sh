0f66f8292b164dfe1d78f6fc86b6d0698de91b5e
lanceman999 <loconn13@jh.edu>
2025-10-13 12:11:57 -0400

    updated code for CGC1

--- scripts/misc_scripts/GFF_csqFormat_updated.R
+++ scripts/misc_scripts/GFF_csqFormat_updated.R
@@ -23,9 +23,10 @@ update_ct <- gff_ct %>%
 
 
 
-
 # C. briggsae
-gff_cb <- ape::read.gff("/vast/eande106/projects/Lance/THESIS_WORK/variant_annotation/raw_data/gff_corrections/QX1410.update.April2025.gff")
+# gff_cb <- ape::read.gff("/vast/eande106/projects/Lance/THESIS_WORK/variant_annotation/raw_data/gff_corrections/QX1410.update.April2025.gff")
+gff_cb <- ape::read.gff("/vast/eande106/projects/Nicolas/fix_anno_ncbi/briggsae/c_briggsae_20250620_curated.gff")
+
 
 update_cb <- gff_cb %>%
   dplyr::mutate(attributes = ifelse(type == "mRNA" | type == "CDS", gsub("product=","locus=", attributes), attributes)) %>%
@@ -40,4 +41,25 @@ update_cb <- gff_cb %>%
   dplyr::mutate(attributes = ifelse(seqid == "MtDNA" & type == "gene", gsub("ID=", "ID=gene:", attributes), attributes))
 
 
-# write.table(update_cb,"/vast/eande106/projects/Lance/THESIS_WORK/variant_annotation/raw_data/gff_corrections/QX1410.update.April2025.csq.gff3",quote = FALSE, row.names = FALSE, sep = '\t', col.names = FALSE)
+write.table(update_cb,"/vast/eande106/data/c_briggsae/genomes/QX1410_nanopore/Feb2020/csq/QX1410.update.June2025.noWBGeneID.csq.gff3",quote = FALSE, row.names = FALSE, sep = '\t', col.names = FALSE)
+
+
+
+# CGC1
+gff_CGC <- ape::read.gff("/vast/eande106/data/c_elegans/genomes/CGC1/gene_models/CGC1.augustus.gff3")
+
+update_CGC1 <- gff_CGC %>%
+  dplyr::mutate(attributes = ifelse(type == "mRNA" | type == "CDS", gsub("product=","locus=", attributes), attributes)) %>%
+  dplyr::mutate(attributes = gsub("hypothetical protein", "hypothetical_protein", attributes)) %>%
+  dplyr::mutate(attributes = ifelse(type == "gene" | type == "mRNA", paste0(attributes,";biotype=protein_coding"),attributes)) %>%
+  dplyr::mutate(phase = as.character(phase)) %>%
+  dplyr::mutate(score = as.character(score)) %>%
+  dplyr::mutate(phase = ifelse(is.na(phase), ".", phase)) %>%
+  dplyr::mutate(score = ifelse(is.na(score), ".", score)) %>%
+  dplyr::mutate(attributes = ifelse(seqid == "MtDNA" & type == "CDS", gsub("ID=", "ID=CDS:", attributes), attributes)) %>%
+  # dplyr::mutate(attributes = ifelse(seqid == "MtDNA" & type == "CDS", gsub("Parent=", "Parent=transcript:", attributes), attributes)) %>%
+  dplyr::mutate(attributes = ifelse(seqid == "MtDNA" & type == "gene", gsub("ID=", "ID=gene:", attributes), attributes)) %>%
+  dplyr::mutate(seqid = gsub("chr","", seqid))
+
+
+write.table(update_CGC1,"/vast/eande106/data/c_elegans/genomes/CGC1/gene_models/csq/CGC1.cleaned.Oct2025.csq.gff3", quote = FALSE, row.names = FALSE, sep = '\t', col.names = FALSE)
